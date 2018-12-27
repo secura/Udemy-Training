e2e35b105068d6a12f2adb15ccdef6e5b81144e1
Gayle <gaylelcaldwell@gmail.com>
2018-12-26 18:52:52 -0600

    Udemy code from training

---
+++ src/Lab/Lab2_StudentDB.java
@@ -0,0 +1,23 @@
+package Lab;
+
+public class Lab2_StudentDB {
+
+	public static void main(String[] args) {
+	// create student
+		Student stu1 = new Student("William", "112223333");
+
+	}
+
+}
+
+class Student{
+	// properties
+			private String name;
+			private String ssn; 
+			
+	public Student(String student, String ssn){
+		this.name = name;
+		this.ssn = ssn;
+	}
+}
+
---
+++ src/basics/Functions.java
@@ -0,0 +1,25 @@
+package basics;
+
+public class Functions {
+
+	public static void main(String[] args) {
+		//Write a function that takes a value n returns the sum of numbers 1 to n
+		System.out.println(sum(5));
+	}
+	public static int sum(int x) {
+		int sum = 0;
+		for (int y = 1; y <= x; y++) {
+			sum = sum + y;
+		}
+	return sum;
+		}
+
+		
+		//Write a function that computes the factorial value
+		
+		//Write a function that returns the sum, max, min, med
+
+
+	
+
+}
